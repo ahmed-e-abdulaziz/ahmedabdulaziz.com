ef7e5d2d61af0bcc235a53c97b7918f84dda36c0
Ahmed Ehab <ahmed.ehab5010@gmail.com>
2021-02-15 09:31:31 +0200

    remove console.log from client side

--- cms/netlify/build/generate-category-api.js
+++ cms/netlify/build/generate-category-api.js
@@ -7,16 +7,10 @@ const generateCategoryApi = () => {
   const allFile = `${apiDir}/categories.json`
   const metaFile = `${apiDir}/categories-meta.json`
   const pages = siteConfig.categories.perPage
-  const doneMessage = `{totalNumber} categories generated in categories API`
 
-  createAll(contentDir, allFile, apiDir)
-    .then((all) => {
-      const totalPages = createPagination(pages, all, `${apiDir}/categories`)
-      createMeta({ totalPages: totalPages }, metaFile)
-      console.log(doneMessage.replace('{totalNumber}', all.length))
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  createAll(contentDir, allFile, apiDir).then((all) => {
+    const totalPages = createPagination(pages, all, `${apiDir}/categories`)
+    createMeta({ totalPages: totalPages }, metaFile)
+  })
 }
 export default generateCategoryApi
--- cms/netlify/build/generate-post-api.js
+++ cms/netlify/build/generate-post-api.js
@@ -7,16 +7,10 @@ const generatePostApi = () => {
   const allFile = `${apiDir}/posts.json`
   const metaFile = `${apiDir}/posts-meta.json`
   const pages = siteConfig.posts.perPage
-  const doneMessage = `{totalNumber} posts generated in posts API`
 
-  createAll(contentDir, allFile, apiDir)
-    .then((all) => {
-      const totalPages = createPagination(pages, all, `${apiDir}/posts`)
-      createMeta({ totalPages: totalPages }, metaFile)
-      console.log(doneMessage.replace('{totalNumber}', all.length))
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  createAll(contentDir, allFile, apiDir).then((all) => {
+    const totalPages = createPagination(pages, all, `${apiDir}/posts`)
+    createMeta({ totalPages: totalPages }, metaFile)
+  })
 }
 export default generatePostApi
