5b74a7dc0fb5fecac44257bd5619d8db4b7d39fb
Ahmed Ehab <ahmed.ehab5010@gmail.com>
2021-02-16 10:37:30 +0200

    add new home hero jopg

--- components/SiteHero.vue
+++ components/SiteHero.vue
@@ -121,7 +121,7 @@ export default {
     left: 0;
     right: 0;
     bottom: 0;
-    background: rgba(0, 0, 0, 0.75);
+    background: rgba(0, 0, 0, 0.65);
     position: absolute;
   }
   .hero-body {
